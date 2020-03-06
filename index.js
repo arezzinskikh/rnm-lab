725f8787d2e1a5304d4e3f7711b4c058e2827e10
MaccabiDev <37658179+MaccabiDev@users.noreply.github.com>
2020-03-06 12:58:28 +0200

    My secondt commit

--- index.js
+++ index.js
@@ -4,16 +4,25 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-mongoose.Promise = global.Promise;
-mongoose.connect(
-  process.env.MONGODB_URI ||
-    `mongodb+srv://nodejs_app:q1a1z1w2@nodejs0-0xmxj.gcp.mongodb.net/test?retryWrites=true&w=majority`
-);
-9
+// IMPORT CONFIG
+require('./config/serverConfig');
+require('./config/mongodbConfig');
+
+// IMPORT MODELS
+require("./models/User");
+
+// IMPORT ROUTES
+require("./routes/userRoutes")(app);
+
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-    console.log(`App running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
 });
